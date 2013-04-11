c24f813eacb26ff0046beec3b3836657beb0e7d7
Andrew <asong641@gmail.com>
2013-04-10 23:23:58 -0500

    Made an ingredients class. Login. Can't figure out though how to associate a user with their own ingredients. Right now the database is shared amongst every user

--- main.py
+++ main.py
@@ -1,18 +1,56 @@
-#!/usr/bin/env python
+import datetime
+import webapp2
+import wsgiref
+from google.appengine.ext import db
+from google.appengine.api import users
 
-import wsgiref.handlers
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp \
-	import template
+class Ingredient(db.Model):
+    
+    name = db.StringProperty(required = True)
+    quantity = db.IntegerProperty()  #should be number or string?
+    date = db.DateTimeProperty(auto_now_add=True)
 
-class MyHandler(webapp.RequestHandler):
-	def get(self):
-		#self.response.out.write(template.render('index.html', {}))
-		self.response.out.write("hi")
-
-def main():
-	app = webapp.WSGIApplication([(r'.*', MyHandler)], debug=True)
-	wsgiref.handlers.CGIHandler().run(app)
+def user_key(name = None):
+	return db.Key.from_path('Ingredient',name)
 	
-if __name__ == "__main__":
-	main()
+class UserPage(webapp2.RequestHandler):
+
+    def get(self):
+        user = users.get_current_user()
+
+        if user:
+            self.redirect("/list")
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
+
+
+# Ingred0 = Ingredient(name = 'water', quantity = 2)
+# Ingred1 = Ingredient(name = 'flour', quantity = 3)
+
+# Ingred0.put()
+# Ingred1.put()
+
+class MainPage(webapp2.RequestHandler):
+
+    def get(self):
+		
+		ingredList = db.GqlQuery("SELECT * "
+								"FROM Ingredient"
+								" ORDER BY when DESC")
+								
+	
+		self.response.write("<b>Ingredient List:</b> <br></br>")
+		for item in ingredList:
+			if item.name:
+				self.response.write('Ingredient: %s Quantity: %s Date: %s <br></br>' %(item.name ,item.quantity ,item.date))
+			else:
+				self.response.write('ERROR no name entered')
+
+# i = Ingredient.all()
+# for x in i:
+	# x.delete()
+
+app = webapp2.WSGIApplication([('/', UserPage),
+                               ('/list', MainPage)],
+                              debug=True)
+wsgiref.handlers.CGIHandler().run(app)
